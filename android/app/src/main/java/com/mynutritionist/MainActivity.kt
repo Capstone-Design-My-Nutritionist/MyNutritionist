f9c957de6ce257a034a2c9698c0bed2a13f20207
HoYeongSung <seonghoyeong@seonghoyeong-ui-MacBookPro.local>
2025-02-22 13:01:15 +0900

    fix build error : react-native-screens

--- android/app/src/main/java/com/mynutritionist/MainActivity.kt
+++ android/app/src/main/java/com/mynutritionist/MainActivity.kt
@@ -4,9 +4,13 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import android.os.Bundle // ← 이 줄 추가
+
 
 class MainActivity : ReactActivity() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(null) // ← 이 줄 추가
+    }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
